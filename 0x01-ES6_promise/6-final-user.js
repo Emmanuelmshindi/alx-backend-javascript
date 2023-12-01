6fb1550829e9d90bcec765cf684caef2c1e00ead
Emmanuelmshindi <mshindimanuel@gmail.com>
2023-12-01 15:38:20 +0000

    Initial commit

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,15 +2,14 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export function handleProfileSignup(firstName, lastName, fileName) {
-  Promise.all([
-    signUpUser,
-    uploadPhoto
-  ]).then((signUpUserMessage, uploadPhotoMessage) => {
-    [
-      {
-        status,
-	
-      }
-    ]
-  }
-}
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+  
+  return Promise.allSettled([userPromise, photoPromise])
+    .then((results) => {
+      return results.map((result) => ({
+        status: result.status,
+	value: result.status === 'fulfilled' ? result.value : result.reason 
+      }));
+    });
+};

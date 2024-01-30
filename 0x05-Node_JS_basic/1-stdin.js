4c80a7d20204cce68de0759679d4f2497f4da6fd
Emmanuelmshindi <mshindimanuel@gmail.com>
2024-01-30 23:51:54 +0000

    Task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,10 +2,10 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n')
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
+  process.stdout.write(`Your name is: ${name}`);
   process.exit();
 });
 
-process.on('exit', () => {
-  console.log('This important software is now closing');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });

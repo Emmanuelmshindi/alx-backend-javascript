a2211be52467e63e01b1c84d0e60d2ebdbd983e2
Emmanuelmshindi <mshindimanuel@gmail.com>
2024-01-30 23:42:13 +0000

    Task 0

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,4 +1,17 @@
+/**
+ * Logs a message to the console.
+ * @param {string} message - The message to log.
+ * @returns {void}
+ */
+
+
 function displayMessage(message) {
   console.log(message);
 };
+
+/**
+ * Exports the displayMessage function.
+ * @module displayMessage
+ * @function
+ */
 module.exports = displayMessage;

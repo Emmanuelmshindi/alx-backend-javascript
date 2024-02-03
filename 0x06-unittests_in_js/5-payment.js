586ccf792613737fa135a99358de24423f8a6624
Emmanuelmshindi <mshindimanuel@gmail.com>
2024-02-03 07:22:24 +0000

    Update task 5

--- 0x06-unittests_in_js/5-payment.js
+++ 0x06-unittests_in_js/5-payment.js
@@ -1,9 +1,9 @@
 const utils = require('./utils');
 
 const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
-  const total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  const total = utils.calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${total}`);
   return total;
-}
+};
 
 module.exports = sendPaymentRequestToApi;
